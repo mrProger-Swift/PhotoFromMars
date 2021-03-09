edcfbe7d5b114861570268aa6f0796a37c7ef858
MJay <orlovakaterina@mail.ru>
2021-03-09 16:44:21 +0400

    create

---
+++ newtestApp/newtestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  newtestAppApp.swift
+//  newtestApp
+//
+//  Created by Michael Rosliakov on 09.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct newtestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
